297d68a8b5cf8918d02441cc9b28841f6dae1b5c
leo <yyinsomnia@gmail.com>
2015-07-22 02:52:27 +0800

    valid the leap second

--- datetime/leap_sec.php
+++ datetime/leap_sec.php
@@ -4,9 +4,14 @@
 echo strtotime("2015-07-01 07:59:60");
 echo "<br />";
 echo strtotime("2015-07-01 08:00:00");
+echo "<br />";
+echo strtotime("2015-07-01 08:59:60");
+echo "<br />";
+echo strtotime("2015-07-01 09:00:00"); //e it don't find the leap second...
+echo "<br />";
 
 echo "<br />";
-echo date("Y-m-d H:i:s", strtotime("-2 second", strtotime("2015-07-01 08:00:00"))); //obvirously the two time use the same timestamp
+echo date("Y-m-d H:i:s", strtotime("-2 second", strtotime("2015-07-01 08:00:00"))); //obviously the two time use the same timestamp
 
 echo "<br />";
 
