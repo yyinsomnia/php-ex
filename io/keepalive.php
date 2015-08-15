2e4a1533becd7d16f7c7154eff49418124e71e90
yyinsomnia <yyinsomnia@gmail.com>
2015-08-15 17:47:01 +0800

    keepalive test

---
+++ io/keepalive.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 使用wireshark抓包，每一次请求都是新的tcp链接
+ * 本地client port都会递增
+ * 这种写发server开启keep alive没有用
+ */	
+	
+for ($i = 0; $i < 100; $i++) {
+	file_get_contents("http://m.iautos.cn/");
+	sleep(1);
+}
