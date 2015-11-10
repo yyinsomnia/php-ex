b5f4ae424a84f5b8457b55b4d4d16b362766d271
yyinsomnia <yyinsomnia@gmail.com>
2015-11-10 11:50:00 +0800

    algorithm

---
+++ al/fibonacci.php
@@ -0,0 +1,34 @@
+<?php
+
+function fibonacci_fast($i)
+{
+	static $res;
+
+	if (isset($res[$i])) {
+		return $res[$i];
+	} else {
+		if ($i == 0) {
+			$res[0] = 0;
+			return 0;
+		} elseif ($i == 1) {
+			$res[1] = 1;
+			return 1;
+		} else {
+			$res[$i] = fibonacci($i - 1) + fibonacci($i - 2);
+			return $res[$i];
+		}
+	}
+}
+
+function fibonacci_naive($i)
+{
+	if ($i == 0) {
+		return 0;
+	} elseif ($i == 1) {
+		return 1;
+	} else {
+		return fibonacci($i - 1) + fibonacci($i - 2);
+	}
+}
+
+var_dump(fibonacci(30));
