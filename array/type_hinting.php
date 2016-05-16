f2bccfae65cea53c571a4618391ee30a9948759e
iwm <iwm@MacBook-Pro.local>
2016-05-16 17:58:02 +0800

    array is just array , object implement arrayaccess can't hinting

---
+++ array/type_hinting.php
@@ -0,0 +1,41 @@
+<?php
+class obj implements ArrayAccess {
+    private $container = array();
+
+    public function __construct() {
+        $this->container = array(
+            "one"   => 1,
+            "two"   => 2,
+            "three" => 3,
+        );
+    }
+
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->container[$offset]);
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->container[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+}
+
+
+function foo(array $p) {
+    return $p['one'];
+}
+
+$o = new obj();
+
+var_dump(foo($o)); //Argument 1 passed to foo() must be of the type array, object given
