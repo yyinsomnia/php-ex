0e2b32216ca18f51b2125486b121fef7538eb6bf
leo <yyinsomnia@gmail.com>
2016-06-04 23:10:03 +0800

    long ago for houzi

--- charset/filter_word.php
+++ charset/filter_word.php
@@ -7,13 +7,16 @@
  */
 
 $handle_source = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'word.txt', 'r');
-$handle_destination = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'word_filted.txt', 'a');
+$handle_destination = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'word_filter.txt', 'a');
 
 while (($line = fgets($handle_source)) !== false ) {
     $words = explode(',', $line);
     preg_match('#[\x{4e00}-\x{9fa5}0-9a-zA-Z]+#u', $words[0], $matches);
 
-    $line_filter = $matches[0] . ',' . intval($words[1]);
+    $line_filter = $matches[0] . ',' . intval($words[1]) . "\r\n";
 
+    if (strlen($line_filter) > 10) {
+        continue;
+    }
     fwrite($handle_destination, $line_filter);
 }
