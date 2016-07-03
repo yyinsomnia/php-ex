57e40adbb2219bc7b957d0e7a11f44de66056868
leo <yyinsomnia@gmail.com>
2016-07-03 09:08:52 +0800

    namespace search

---
+++ ns/class_search.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ns;
+
+class Foo extends stdClass
+{
+
+}
+
+//Fatal error: Class 'ns\stdClass' not found
---
+++ ns/function_search.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ns;
+
+function foo($str) {
+    return strlen($str);
+}
+
+echo foo('sdf');
+
+//return 3
